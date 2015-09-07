9e0110d988b6aca498db750624b153442a7a6444
Chris Meyer <cdmeyer@umn.edu>
2015-09-07 15:08:47 -0500

    Formatted times in AM/PM

--- js/controllers.js
+++ js/controllers.js
@@ -23,10 +23,9 @@ eventControllers.controller("ListController", ['$scope','$http','$interval','FEE
         curItem.location = "Not Available"
       }
 
-
       //Format the date and time
       var eventDate;
-      console.log("date and time before format: "+curItem.date + " " + curItem.start_time);
+      console.log("date and time for " + curItem.event_name+ " before format: "+curItem.date + " " + curItem.start_time + " - " + curItem.end_time);
       //First format the date for display
       var displayDate = curItem.date.split('-');
       displayDate = new Date(parseInt(displayDate[0],10),parseInt(displayDate[1],10)-1,parseInt(displayDate[2],10));
@@ -35,47 +34,34 @@ eventControllers.controller("ListController", ['$scope','$http','$interval','FEE
       //Now format the start time and end time
       if(curItem.start_time == "00:00:00"){
         console.log("start time NOT set");
-        //eventDate = curItem.date.split('-');
-        //eventDate = new Date(parseInt(eventDate[0],10),parseInt(eventDate[1],10)-1,parseInt(eventDate[2],10));
-
-        //Just show the date if a time is not set
-        //Change YYYY-MM-DD to MM/DD
-        //displayDate = ((eventDate.getMonth()+1) + "/" + eventDate.getDate());
         curItem.start_time = "?";
-
-        //console.log("start date: " + eventDate.toDateString() );
       }
       else{
         console.log("start time IS set");
         //Show the date and time as MM/DD HH:MM AM/PM
-        eventDate = new Date(curItem.date + 'T' + curItem.start_time);
-
-        //curItem.date = ((eventDate.getMonth()+1) + "/" + eventDate.getDate());
-        curItem.start_time = (eventDate.getHours() + ":" + eventDate.getMinutes());
-
-        //console.log("start date: " + eventDate.toLocaleDateString() );
+        eventDate = new Date(curItem.date + 'T' + curItem.start_time+"-0500");
+        curItem.start_time = formatAMPM(eventDate);
       }
 
       if(curItem.end_time == "00:00:00"){
         curItem.end_time = "?";
       }
       else{
-        eventDate = new Date(curItem.date + 'T' + curItem.end_time);
-        curItem.end_time = (eventDate.getHours() + ":" + eventDate.getMinutes());
+        eventDate = new Date(curItem.date + 'T' + curItem.end_time+"-0500");
+        curItem.end_time = formatAMPM(eventDate);
       }
       //Set the date format now that the time has been formatted
       curItem.date = displayDate;
-
 
     }
 
     $scope.events = resItems;
     //Set default sort field to name
-    $scope.eventOrder = 'event_name';
+    $scope.eventOrder = 'date';
     //Get limit from the config
     $scope.eventLimit = FEED_CONFIG.itemLimit;
 
-    $interval(swapItems, FEED_CONFIG.swapFrequency);
+    //$interval(swapItems, FEED_CONFIG.swapFrequency);
     //setTimeout(swapItems,5000);
   });
 }]);
@@ -103,4 +89,17 @@ function swapItems(){
   //console.log('hidEl ID: '+hidEl.attr("id"));
   hidEl.removeClass("hidden");
   hidEl.addClass("show");
+}
+/*
+* Lifted from http://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+*/
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
 }
