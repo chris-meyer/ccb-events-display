cd5602ed0eac1ad133d41739106661896d5a50cb
Chris Meyer <cmeyer8998@gmail.com>
2017-11-28 21:34:36 -0600

    Fixed announcement image path for img src

--- php/findimages.php
+++ php/findimages.php
@@ -3,25 +3,16 @@ $ccbConf = parse_ini_file(realpath(__DIR__.'/../ccb-events.conf'));
 
 $dir = realpath($ccbConf['slide_img_path']);
 
-//Get the base to use to create the img src value
-$imgBase = $_SERVER['REQUEST_URI'];
-$imgBase = substr($imgBase,0,strpos($imgBase,'/php/findimages'));
-
 //The full system path to the web-accessible images
-$annImgPath = realpath(__DIR__.'../images/announcements');
-
-/*
- * This should work ???
- */
-//$imgBase = explode('/',$imgBase);
-//$imgBase = $imgBase[0];
+$annImgPath = realpath(__DIR__.'/../images/announcements');
 
 $annList = array();
 // Open a known directory, and proceed to read its contents
 if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
         while (($file = readdir($dh)) !== false) {
-            if(filetype($dir .'\\'. $file) == 'file' && (preg_match("/(\.png|\.jpeg|\.jpg|\.gif)$/",$file) != FALSE) ){
+
+            if(filetype($dir .'/'. $file) == 'file' && (preg_match("/(\.png|\.jpeg|\.jpg|\.gif)$/",$file) != FALSE) ){
               //Set the image link for this file
               //Check if that image exists in the web announcements folder
               if(!file_exists($annImgPath.'/'.$file)){
@@ -29,7 +20,7 @@ if (is_dir($dir)) {
                 //copy();
               }
               //TODO: How to determine images/announcements programmatically?
-              $annList[] = ($imgBase.'/images/announcements/'.$file);
+              $annList[] = ('images/announcements/'.$file);
             }
         }
         sort($annList);
