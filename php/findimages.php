6a9f30442c100c23eb7c3c376e2af0415d835ed9
Chris Meyer <cmeyer8998@gmail.com>
2017-11-28 21:41:10 -0600

    Images are copied into images/announcements

--- php/findimages.php
+++ php/findimages.php
@@ -5,7 +5,7 @@ $dir = realpath($ccbConf['slide_img_path']);
 
 //The full system path to the web-accessible images
 $annImgPath = realpath(__DIR__.'/../images/announcements');
-
+//The list of image paths used for the img src
 $annList = array();
 // Open a known directory, and proceed to read its contents
 if (is_dir($dir)) {
@@ -13,13 +13,12 @@ if (is_dir($dir)) {
         while (($file = readdir($dh)) !== false) {
 
             if(filetype($dir .'/'. $file) == 'file' && (preg_match("/(\.png|\.jpeg|\.jpg|\.gif)$/",$file) != FALSE) ){
-              //Set the image link for this file
               //Check if that image exists in the web announcements folder
               if(!file_exists($annImgPath.'/'.$file)){
                 //Copy the file into the announcements folder
-                //copy();
+                copy( ($dir .'/'. $file), ($annImgPath.'/'.$file) );
               }
-              //TODO: How to determine images/announcements programmatically?
+              //Add the image link for this file to the list
               $annList[] = ('images/announcements/'.$file);
             }
         }
