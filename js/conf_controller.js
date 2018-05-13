a8a75389bce71ed09d5883a4d2bfb23bf9dd63c6
Chris Meyer <cmeyer8998@gmail.com>
2018-05-13 13:42:52 -0500

    Fade-in message works. Working on fade-out

--- js/conf_controller.js
+++ js/conf_controller.js
@@ -8,6 +8,8 @@ var confController = angular.module("confController", ['ngAnimate']);
 */
 confController.controller("ConfController", ['$scope','$http','feedConfigService', function ($scope, $http, feedConfigService){
   $scope.conf = {};
+  $scope.showMessages = false;
+
 
   //Attempt to get the configuration
   feedConfigService.getConfig()
@@ -63,7 +65,13 @@ confController.controller("ConfController", ['$scope','$http','feedConfigService
             console.log("saveSuccess");
             console.log(response);
             //TODO: Use the response code to show the errors
-
+            $scope.messages = 'Config settings saved!';
+            $scope.showMessages = true;
+            setTimeout(function(){
+              $scope.showMessages = false;
+              $scope.messages = false;
+              console.log('hiding messages');
+            },5000);
           },
           function saveFailure(response){
             console.log("saveFailure");
